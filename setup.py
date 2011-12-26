caad60119de683faa40e20b6acbc29799b82d84e
jpic <jamespic@gmail.com>
2011-12-26 23:45:14 +0100

    Renaming package from subscription to social

--- setup.py
+++ setup.py
@@ -4,18 +4,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-subscription',
-    version="0.0",
-    author='Steve Yeago',
-    author_email='subsume@gmail.com',
-    description='Managing subscriptions in Django',
-    url='http://github.com/subsume/django-subscription',
+    version='0.0',
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    description='Django app with facebook-like features',
+    url='http://github.com/yourlabs/django-social',
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development"
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
     ],
 )
